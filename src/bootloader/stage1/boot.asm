4bada38202f2832b37738053adfc783b41a6a912
ElTitoDG <julianhinojosagil@gmail.com>
2022-07-27 23:19:46 +0200

    Hello world from C

--- src/bootloader/stage1/boot.asm
+++ src/bootloader/stage1/boot.asm
@@ -115,7 +115,7 @@ start:
     mov di, buffer
 
 .search_kernel:
-    mov si, file_kernel_bin
+    mov si, file_stage2_bin
     mov cx, 11                          ; compare up to 11 characters
     push di
     repe cmpsb
@@ -134,7 +134,7 @@ start:
 
     ; di should have the address to the entry
     mov ax, [di + 26]                   ; first logical cluster field (offset 26)
-    mov [kernel_cluster], ax
+    mov [stage2_cluster], ax
 
     ; load FAT from disk into memory
     mov ax, [bdb_reserved_sectors]
@@ -151,7 +151,7 @@ start:
 .load_kernel_loop:
 
     ; read next cluster
-    mov ax, [kernel_cluster]
+    mov ax, [stage2_cluster]
 
     ; not nice :( hardcoded value
     add ax, 31                          ; first cluster = (kernel_cluster - 2) * sectors_per_cluster + start_sector
@@ -163,7 +163,7 @@ start:
     add bx, [bdb_bytes_per_sector]
 
     ; compute location of next cluster
-    mov ax, [kernel_cluster]
+    mov ax, [stage2_cluster]
     mov cx, 3
     mul cx
     mov cx, 2
@@ -187,7 +187,7 @@ start:
     cmp ax, 0x0FF8                      ; end of chain
     jae .read_finish
 
-    mov [kernel_cluster], ax
+    mov [stage2_cluster], ax
     jmp .load_kernel_loop
 
 .read_finish:
@@ -247,7 +247,7 @@ floppy_error:
     jmp wait_key_and_reboot
 
 kernel_not_found_error:
-    mov si, msg_kernel_not_found
+    mov si, msg_stage2_not_found
     call puts
     jmp wait_key_and_reboot
 
@@ -359,9 +359,9 @@ disk_reset:
 
 msg_loading:            db 'Loading...', ENDL, 0
 msg_read_failed:        db 'Read from disk failed!', ENDL, 0
-file_kernel_bin:        db 'STAGE2  BIN'
-msg_kernel_not_found:   db 'STAGE2.BIN file not found!', ENDL, 0
-kernel_cluster:         dw 0
+file_stage2_bin:        db 'STAGE2  BIN'
+msg_stage2_not_found:   db 'STAGE2.BIN file not found!', ENDL, 0
+stage2_cluster:         dw 0
 
 KERNEL_LOAD_SEGMENT     equ 0x2000
 KERNEL_LOAD_OFFSET      equ 0
